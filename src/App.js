48f0b913bea50a530dbbb5890a6b8b7f22a16c03
supprogramer <agito11752@gmail.com>
2020-07-30 05:51:10 -0500

    login google and facebook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import GoogleLogin from 'react-google-login';
+import Facebook from './Components/Facebook';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+
+  responseGoogle=(response)=>{
+    console.log(response);
+    console.log(response.profileObj);
+    
+    
+  }
+  render() {
+    return (
+      <div>
+        <GoogleLogin
+        clientId="676827053885-l28q13lsa1snu3qm80c408ektc0hqk45.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={this.responseGoogle}
+        onFailure={this.responseGoogle}
+        cookiePolicy={'single_host_origin'}/>
+
+        
+        <Facebook/>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
